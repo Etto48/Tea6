e08b566e5e17381dc2d30a13a959a5342c360fbc
etto48 <minecraft.etto@gmail.com>
2021-11-17 01:23:26 +0100

    index

--- Index/Index.h
+++ Index/Index.h
@@ -5,10 +5,15 @@
 
 namespace Index
 {
+    constexpr unsigned int HASH_TABLE_SIZE = 256;
     class Index
     {
     protected:
+        _IndexList hashTable[HASH_TABLE_SIZE];
     public:
+        bool operator+=(const UserData& entry);
+        bool operator-=(const std::string& username);
+        UserData* find(const std::string& username);
     };
 
     struct UserData
